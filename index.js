c6f7b1f6319df93fb734b5b802ab4e26022b9c97
muzammel hussain <muzammelhussain533@gmail.com>
2025-12-11 00:05:14 +0600

    mongodb is connected with server

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
@@ -7,6 +9,57 @@ const port = process.env.PORT || 3000;
 //middleware
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@keramot.mqb48yw.mongodb.net/?appName=Keramot`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("loan_link_db");
+    const usersCollection = db.collection("users");
+
+    // app.post("/users", async (req, res) => {
+    //   const user = req.body;
+    //   user.role = "user";
+    //   user.createdAt = new Date();
+    //   const email = user.email;
+    //   const userExists = await usersCollection.findOne({ email });
+
+    //   if (userExists) {
+    //     return res.send({ message: "user exists" });
+    //   }
+
+    //   const result = await usersCollection.insertOne(user);
+    //   res.send(result);
+    // });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("loan-link server is running!!!!!");
